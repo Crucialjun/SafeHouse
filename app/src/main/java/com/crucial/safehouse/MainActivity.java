ccfbe56dfb4e211574b93123ffa2fc0cb324b39a
Nicholas <nich.otieno@gmail.com>
2019-07-20 21:33:28 +0300

    Add Sign in Button

--- app/src/main/java/com/crucial/safehouse/MainActivity.java
+++ app/src/main/java/com/crucial/safehouse/MainActivity.java
@@ -1,13 +1,42 @@
 package com.crucial.safehouse;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 7117;
+    List<AuthUI.IdpConfig> providers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+                
+        );
+        
+        showSignInOptions();
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),MY_REQUEST_CODE
+        );
     }
 }
