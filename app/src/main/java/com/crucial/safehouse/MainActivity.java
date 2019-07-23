17e8e9f00ad71e77aa8fa3d5fcfb1608d21efa7e
Nicholas <nich.otieno@gmail.com>
2019-07-23 20:53:20 +0300

    Add Sign in Button

--- app/src/main/java/com/crucial/safehouse/MainActivity.java
+++ app/src/main/java/com/crucial/safehouse/MainActivity.java
@@ -24,26 +24,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_logout = findViewById(R.id.btn_sign_out);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AuthUI.getInstance()
-                        .signOut(MainActivity.this)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        btn_logout.setEnabled(true);
-                        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
-                        startActivity(intent);
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
+        btn_logout.setOnClickListener(v -> AuthUI.getInstance()
+                .signOut(MainActivity.this)
+                .addOnCompleteListener(task -> {
+                    btn_logout.setEnabled(true);
+                    Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }).addOnFailureListener(e -> Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show()));
         
 
     }
